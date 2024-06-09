943b4a95325769b57d35710b9654f64cd0f86da3
minhducthanhhoa <ngominhduc2705@gmail.com>
2024-06-09 22:33:23 +0700

    bai tap ss55

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>App
+      {/*
+        API: application programing interface
+        giúp cho các ứng dụng có thể giao tiếp với database, tương tác với cơ sở dữ liệu 
+       */}
+    </div>
+  )
+}
